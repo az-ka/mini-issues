bee1038522cb7c54eff4a0e1113e41710205171a
azka <fadlian.dev@gmail.com>
2026-03-14 12:48:00 +0700

    feat: add favicon assets and web manifest for PWA support
    - Add favicon.ico and multiple resolution PNGs for various platforms
    - Include Android Chrome icons and Apple touch icon
    - Add web manifest file for PWA installation support
    - Update HTML head to reference new favicon assets instead of SVG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { ClerkProvider } from 'svelte-clerk/client';
 	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
 	import ConvexClientProvider from '$lib/components/ConvexClientProvider.svelte';
@@ -10,7 +9,6 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
 	<title>Mini Issues</title>
 </svelte:head>
 
